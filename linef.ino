8242395888d385799a57070390a17f138e28015e
vishwak <49344980+vishwakneelamegam@users.noreply.github.com>
2019-04-07 09:34:07 +0530

    Add files via upload

---
+++ linef.ino
@@ -0,0 +1,53 @@
+//by vishwak
+#define s1 6
+#define s2 7
+#define m1 2
+#define m2 3
+#define m3 4
+#define m4 5
+String straight()
+{
+  digitalWrite(m1,HIGH);
+  digitalWrite(m2,LOW);
+  digitalWrite(m3,HIGH);
+  digitalWrite(m4,LOW);
+  return "STRAIGHT";
+}  
+String left()
+{
+  digitalWrite(m1,LOW);
+  digitalWrite(m2,HIGH);
+  digitalWrite(m3,HIGH);
+  digitalWrite(m4,LOW);
+  return "LEFT";
+} 
+String right()
+{
+  digitalWrite(m1,HIGH);
+  digitalWrite(m2,LOW);
+  digitalWrite(m3,LOW);
+  digitalWrite(m4,HIGH);
+  return "RIGHT";
+} 
+
+void setup()
+{
+  pinMode(s1,INPUT);
+  pinMode(s2,INPUT);
+  }
+void loop()
+{
+  if(digitalRead(s1)==1 && digitalRead(s2)==1)
+  {
+    straight();
+    } 
+    if(digitalRead(s1)==0 && digitalRead(s2)==1)
+  {
+    right();
+    } 
+    if(digitalRead(s1)==1 && digitalRead(s2)==0)
+  {
+    left();
+    } 
+}
+
